bbcdd2f3db5131dcac7a92f978bee47e118be205
Eva Ezekiel <eva.kiel7@gmail.com>
2022-09-12 21:10:15 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers 0-9
+ *
+ * Return: Always successful
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0)
+}
