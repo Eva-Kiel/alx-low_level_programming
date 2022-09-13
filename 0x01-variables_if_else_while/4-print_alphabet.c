aee34e90b88b291c4b68dd1560b85a7330fb0a3e
Eva Ezekiel <eva.kiel7@gmail.com>
2022-09-12 21:35:33 -0700

    Alphabet Soup

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h.
+
+/**
+ * main - Remove some alphabets
+ *
+ *Return: Always succesful
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = a'; ch <= 'z'; c++)
+	{
+		if (c != 'e'&& c != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
