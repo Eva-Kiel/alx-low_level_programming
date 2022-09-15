8ffec7718e5291b8599419f8c62d894bb0037ece
Eva Ezekiel <eva.kiel7@gmail.com>
2022-09-14 22:08:53 -0700

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+
+#include "main.h"
+/**
+ * jack_bauer - function prints every minute of Jack Bauer
+ * starting from 00:00 to 23:59
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+	int minutes = 0;
+	int hours_remainder;
+	int mins_remainder;
+
+	while (hours <= 23)
+	[
+	while (minutes <= 59)
+	{
+	mins_remainder = minutes % 10;
+	hours_remainder = hours % 10;
+	_putchar (hours_remainder + '0');
+
