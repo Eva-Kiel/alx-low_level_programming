e71afada327485e860a015109d92283251310038
Eva Ezekiel <eva.kiel7@gmail.com>
2022-09-20 20:36:01 -0700

    put half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half a string, followed by new line
+ *
+ * @str: input string
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int index, half;
+
+	index = 0;
+	while (str[index] != '\0')
+		index++;
+
+	half = index / 2;
+
+	if (index % 2 == 1)
+		half++;
+
+	while (half < index)
+	{
+		putchar(str[half]);
+		half++;
+	}
+	putchar('\n');
+}
