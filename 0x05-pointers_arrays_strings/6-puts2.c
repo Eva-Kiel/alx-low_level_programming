592accddb4aba35177fe3b4a0f5c5a15e264040c
Eva Ezekiel <eva.kiel7@gmail.com>
2022-09-20 21:34:26 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character in a string
+ * @str: string
+ *
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+		i++;
+	}
+	putchar('\n');
+}
