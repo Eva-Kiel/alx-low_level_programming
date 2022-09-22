b5152af578fec2d0b40dfd150b2f707cd3f3b1d7
Eva Ezekiel <eva.kiel7@gmail.com>
2022-09-22 06:53:31 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first parameter
+ * @s2: second parameter
+ *
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+
+	if (s1[i] === s2[i])
+		return (0);
+	return (s1[1] - s2[i]);
+}
