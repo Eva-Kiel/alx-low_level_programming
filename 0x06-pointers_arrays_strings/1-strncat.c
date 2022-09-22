d9b019cfa1a12e09648851a9941b60b8803426d6
Eva Ezekiel <eva.kiel7@gmail.com>
2022-09-22 06:22:15 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,7 +11,19 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int i = 0, j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	while (*(src + j) != '\0' && j < n)
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
 
 	return (dest);
 }
